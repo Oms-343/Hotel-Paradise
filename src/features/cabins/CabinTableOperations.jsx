6626890bf76108bbc89f7b543f999befd12e993e
Oms-343 <oms87473@gmail.com>
2024-09-21 14:05:46 +0530

    added client side sorting for the cabins

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,6 +1,7 @@
 import { RxValue } from "react-icons/rx";
 import Filter from "../../ui/Filter";
 import TableOperations from "../../ui/TableOperations";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
@@ -19,6 +20,34 @@ function CabinTableOperations() {
           {
             value: "with-discount",
             label: "with discount",
+          },
+        ]}
+      />
+      <SortBy
+        options={[
+          {
+            value: "name-asc",
+            label: "sort by name (A-Z)",
+          },
+          {
+            value: "name-desc",
+            label: "sort by name (Z-A)",
+          },
+          {
+            value: "regularPrice-asc",
+            label: "sort by price (low first)",
+          },
+          {
+            value: "regularPrice-desc",
+            label: "sort by price (high first)",
+          },
+          {
+            value: "maxCapacity-asc",
+            label: "sort by capacity (low first)",
+          },
+          {
+            value: "maxCapacity-desc",
+            label: "sort by capacity (high first)",
           },
         ]}
       />
